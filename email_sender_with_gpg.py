bbcd9368605b42efbd1d6435e55ee99ddde4976e
Aknik <kinka@tutanota.de>
2017-02-25 21:30:12 +0100

    Update email_sender_with_gpg.py
    Ahora encuentra el fingerprint sabiendo el email de destino

--- email_sender_with_gpg.py
+++ email_sender_with_gpg.py
@@ -1,34 +1,40 @@
 import smtplib
 import gnupg
-to = ""  #insert reciever's email id here
-user_name = "" #insert your email id here
-appcode = "" #insert google app code here or you gmail password
+
+to = "user@8chan.co"  #insert reciever's email id here
+user_name = "user@gmail.com" #insert your email id here
+appcode = "password" #insert google app code here or you gmail password
+
 smtpserver=smtplib.SMTP("smtp.gmail.com",587)
 smtpserver.ehlo()
 smtpserver.starttls()
 smtpserver.ehlo()
 smtpserver.login(user_name,appcode)
-subject = "" #insert subject here
+subject = "gnupg" #insert subject here
 head = 'To:'+to+'\nFrom: '+user_name+'\nSubject : '+subject+'\n'
-body = "" #inser message body here
+body = "cuerpo del mensaje" #inser message body here
+
 #set location of  gpg keys
-gpg=gnupg.GPG(gnupghome="/path/to/.gnupg/")
+gpg=gnupg.GPG(homedir="~/.gnupg/")
 gpg.encoding='utf-8'
+
 #fetch the keys
 pub_keys = gpg.list_keys()
-#removing other data from keys info fetched, need only fingerprint and email
-#thanks to sant0sh for helping me figure out how to do this
-new={}
+
 for i in pub_keys:
-     new.update({i['uids'][0]:i['fingerprint']})
-#TODO : insert logic for selecting which key to use, for now use some key
+# Busca el email TO dentro de i['uids'][0]     
+     if to in i['uids'][0]: 
+         print (i['uids'][0],i['fingerprint'])
+         fingerprint = i['fingerprint']
+
 #encrypt the message body
-enc = gpg.encrypt(body,new["insert key uid here"])
+enc = gpg.encrypt(body,fingerprint)
+encrypted_body = str(enc)
+
 #add the header
-msg = head + enc
+msg = head + encrypted_body
 #send the mail
 smtpserver.sendmail(user_name,to,msg)
-print "done!!"
+print (encrypted_body)
 smtpserver.close()
-
 
